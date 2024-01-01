cb727a9d3f6250385e6320425847e74999cbc7ae
Rami81382610 <152017358+Rami81382610@users.noreply.github.com>
2024-01-01 22:27:34 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/material.dart';
+import 'registration.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background Image
+          Image.asset(
+            'assets/car12.jpg', // Replace with the actual URL
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          // Centered Content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome to the Car Community',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegistrationPage()),
+                    );
+                  },
+                  child: Text('Click here to Register'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
